a3f1b46dad9a17752d186df402b33ccb81483ed1
jdzhang <jdzhang@in-road.com>
2024-12-26 15:21:08 +0800

    feat: 更换字体文件

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,27 @@
+/*
+ * @Author: jdzhang jdzhang@in-road.com
+ * @Date: 2024-12-25 15:08:49
+ * @LastEditors: jdzhang jdzhang@in-road.com
+ * @LastEditTime: 2024-12-26 15:18:28
+ * @FilePath: \url-navigator\tailwind.config.ts
+ * @Description: 
+ */
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ['var(--font-inter)', 'system-ui', '-apple-system', 'PingFang SC', 'Microsoft YaHei', 'sans-serif'],
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
