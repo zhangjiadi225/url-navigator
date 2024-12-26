45fcdfab813545e4639be844cac2b70208a65475
jdzhang <jdzhang@in-road.com>
2024-12-26 16:04:43 +0800

    build: 静态导出

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+/*
+ * @Author: jdzhang jdzhang@in-road.com
+ * @Date: 2024-12-25 17:38:52
+ * @LastEditors: jdzhang jdzhang@in-road.com
+ * @LastEditTime: 2024-12-26 15:55:51
+ * @FilePath: \url-navigator\next.config.mjs
+ * @Description: 
+ */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
+  distDir: 'out',
   images: {
     unoptimized: true
   },
